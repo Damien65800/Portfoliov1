63e5d1bed40125750a1a683b0e67d52a5b6652c5
Damien65800 <65216200+Damien65800@users.noreply.github.com>
2021-02-18 14:59:52 +0100

    new

--- style.js
+++ style.js
@@ -62,11 +62,4 @@ window.onload = function () {
             });
         }
     });
-
-    document.querySelector('#contact-form').addEventListener('submit', function (e) {
-        e.preventDefault();
-        e.target.elements.name.value = '';
-        e.target.elements.email.value = '';
-        e.target.elements.message.value = '';
-    });
 }
